786d07fe8b960c3669b7d7252a4368c6a0979ab4
michaelplott <mike.plott80@gmail.com>
2016-11-08 10:19:05 -0500

    Ants drawn and fps displaying correctly.

---
+++ src/sample/Ant.java
@@ -0,0 +1,13 @@
+package sample;
+
+/**
+ * Created by michaelplott on 11/8/16.
+ */
+public class Ant {
+    double x, y;
+
+    public Ant(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
